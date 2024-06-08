0f692f1d1384222fe037748415d85ae77aa73374
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-06-08 01:26:55 -0700

    creando componentes principales

---
+++ src/app/control/ingresos/ingresos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresos',
+  standalone: true,
+  imports: [],
+  templateUrl: './ingresos.component.html',
+  styleUrl: './ingresos.component.css'
+})
+export class IngresosComponent {
+
+}
